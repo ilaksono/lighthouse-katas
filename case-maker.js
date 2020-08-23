6bc7e2499e9ec72fc5fede098a3a2b46700bf8c6
ilaksono <ian.laksono@gmail.com>
2020-08-23 04:06:34 +0000

    kata 7 - case maker function

---
+++ case-maker.js
@@ -0,0 +1,20 @@
+const camelCase = function(str) {
+  var copy = [];
+  for(let i = 0; i < str.length; i++){
+    if(str[i] === ' ');
+      spaces.push(i);
+  }
+  for(i = 0; i < str.length; i++){
+    if(str[i] === ' '){
+      i++;
+      copy[i] = str[i].toUpperCase();
+    }
+    else
+      copy[i] = str[i]; 
+  }
+  return copy.join('').toString();  
+};
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
