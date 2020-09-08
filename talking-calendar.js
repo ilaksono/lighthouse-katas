05d8da0ccfeb5f321a6cda4629e7a277d5aee9ae
ilaksono <ian.laksono@gmail.com>
2020-09-08 18:28:14 +0000

    fixed bug in talking calendar function

--- talking-calendar.js
+++ talking-calendar.js
@@ -3,10 +3,12 @@ const talkingCalendar = str => {
   let dayDigs = parseInt(str.slice(-2));
   var monthsArray = ['January', 'February','March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   let dayExt = '';
-  if(dayDigs === 1 || dayDigs === 21 || dayDigs === 31)
+  if(dayDigs % 10 === 1 && dayDigs != 11)
     dayExt = 'st';
-  else if(dayDigs === 2 || dayDigs === 22)
+  else if(dayDigs % 10 === 2 && dayDigs != 12)
     dayExt = 'nd';
+  else if(dayDigs % 10 === 3 && dayDigs != 13)  
+    dayExt = 'rd';
   else  
     dayExt = 'th';
   return `${monthsArray[monthDigs - 1]} ${dayDigs}${dayExt}, ${str.slice(0,4)}`; 
@@ -16,3 +18,4 @@ const talkingCalendar = str => {
 console.log(talkingCalendar("2017/12/02"));
 console.log(talkingCalendar("2007/11/11"));
 console.log(talkingCalendar("1987/08/24"));
+console.log(talkingCalendar("1421/08/03"));
